58a9ced743455c7dda7a7bbbf582991a7e2fb497
”motegidaichi” <daichimotegi921@gmail.com>
2020-01-30 01:40:14 +0000

    first commit

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,14 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
+    //
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('profile/edit', 'Admin\ProfileController@edit');
 });
-    Route::get('XXX', 'XXX\AAAController@bbb');
     
-Route::group(['prefix' => 'admin'], function(){
-    Route::get('profile/create', 'admin\ProfileController@add');
-    Route::get('profile/edit', 'admin\ProfileController@edit');
-});
+    
+
 
