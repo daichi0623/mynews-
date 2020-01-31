33b071e0511d6c5cf771d2071972847651348709
”motegidaichi” <daichimotegi921@gmail.com>
2020-01-31 09:47:46 +0000

    new file

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -18,25 +18,25 @@
                     <div class="form-group row">
                         <label class="col-md-2">氏名</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                            <input type="text" class="form-control" name="name" value="{{ old('name') }}">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label class="col-md-2">性別</label>
-                        <div class="col-md-10">
-                            <input type="text" class="form=control" name="title" value="{{ old('title') }}">
-                        </div>
+                            <input type= "radio" name ="gender" value = "man"><b>男性</b>
+                            <input type= "radio" name ="gender" value = "woman"><b>女性</b>
+                        </p>
                     </div>
                     <div class="form=group row">
                             <label class="col-md-2">趣味</label>
                             <div class="col-md-15">
-                            <input type="text" class="form=control" name="title" value="{{ old('title') }}">
+                            <input type="text" class="form=control" name="hobby" value="{{ old('hobby') }}">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label class="col-md-2">自己紹介欄</label>
                         <div class="col-md-10">
-                            <textarea class="form-control" name="body" rows="10">{{ old('body') }}</textarea>
+                            <textarea class="form-control" name="introduction" rows="10">{{ old('introduction') }}</textarea>
                         </div>
                     </div>
                     
