b9d57b64768ec7467cf45f3dd59b28cb2f6cfa3e
”motegidaichi” <daichimotegi921@gmail.com>
2020-01-31 15:23:27 +0000

    new file

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,29 +5,27 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Profiles;
+
 class ProfileController extends Controller
 {
     public function add()
-
     {
         return view('admin.profile.create');
     }
     
-    public function create ()
+    public function create(Request $request)
     {
-        return redirect('admin/profile/create');
-    }
-    
-    
-    public function edit()
-    {
-        return view('admin.profile.edit');
-    }
-    
-    public function update()
-    {
-        return redirect('admin/profile/edit');
+        
+        $this->validate($request, Profiles::$rules);
+        
+        $profiles = new Profiles;
+        $form = $request->all();
+        
     }
     
 }
 
+    
+    
+    
