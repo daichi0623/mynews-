3947ea603cb7594738f9c7aa37a1855a096c2f8f
”motegidaichi” <daichimotegi921@gmail.com>
2020-02-19 06:37:00 +0000

    heroku

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -3,13 +3,10 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
 use App\News;
-
 use App\History;
-
 use Carbon\Carbon;
-
+use Storage;
 class NewsController extends Controller
 {
   public function add()
@@ -26,8 +23,8 @@ class NewsController extends Controller
       $form = $request->all();
       // 
       if ($form['image']) {
-        $path = $request->file('image')->store('public/image');
-        $news->image_path = basename($path);
+        $path = Storage::disk('s3')->putFile('/',$form['image'],'public');
+        $news->image_path = Storage::disk('s3')->url($path);
       } else {
           $news->image_path = null;
       }
@@ -72,8 +69,8 @@ class NewsController extends Controller
         if ($request->remove == 'true') {
             $news_form['image_path'] = null;
         } elseif ($request->file('image')) {
-            $path = $request->file('image')->store('public/image');
-            $news_form['image_path'] = basename($path);
+            $path = Storage::disk('s3')->putFile('/',$form['image'],'public');
+            $news_form['image_path'] = Storage::disk('s3')->url($path);
         } else {
             $news_form['image_path'] = $news->image_path;
         }
